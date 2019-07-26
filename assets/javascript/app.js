6e5edbbc4bc63f3a6fe35cd1f9ff608600dab531
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-26 12:36:03 -0400

    pseudo JS file & some basic HTML. Added bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// Short Document Ready
+$(function () {
+
+
+
+    // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
+    // setTimeout
+    // $(div.submitAnswerButton);
+
+    // Click Handler Function
+    function handleClick() {
+        // something smart to handle clicks and make sure they select one answer from buttons 1-4 and then click submitAnswerButton
+        // If the player selects the correct answer, show a screen congratulating them for choosing the right option. 
+        // After a few seconds, display the next question -- do this without user input.
+        function answerChecker() {
+
+        }
+    }
+
+
+    // The scenario is similar for wrong answers and time - outs.
+    // If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+    // If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer.Wait a few seconds, then show the next question.
+
+
+    // On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+    function finalScreen() {
+        // count the number of correctGuesses, then append the html "correct guesses: correctGuesses".
+        // same for incorrectGuesses
+        // would you like to restart the game?(without reloading the page).
+
+    }
+
+
+});

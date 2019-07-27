0441da09cbf62cb457cfc48d04670483aa67d48a
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-27 13:42:54 -0400

    Got the start button to hide.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,69 @@
 // Short Document Ready
 $(function () {
 
+    // Make some lets here //
+
+
     // Need a function for start btn click event that will call a few other functions into action.
-    // 1st this should hide the start btn
-    // 2nd then call another function named showEverythingQuestion Section.
-    // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
+    // 1st this should hide the start btn // This is working now //
+    $(document).on('click', 'button#startButton', function startGame() {
+        $(this).hide();
+        // 2nd then call another function named showEverythingQuestion Section.
+        // Now add code to show the question section here.....
+        // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
+    });
+
+    // AJAX request //
+    // ORIGINAL //
+    // $.ajax({
+    //     url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
+    //     dataType: 'json',
+    //     method: "GET"
+    // }).then(function (response) {
+    //     // let questionOptions = {
+    //     //     "question 1": "(response.results[`0`].question",
+    //     //     "correct answer": "",
+    //     return (response.results);
+    // });
+
+    // WORKING CODE at the console level //
+    function getdata() {
+        var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            // response.results.forEach(elem => {
+            //     let responseArray = $(); ... took a pciture with my cell phone that might help //
+            // })
+
+        });
+
+    };
+    getdata();
+    // Not working at the global level
+    // console.log(response.results[0].question);
+    // console.log(response.results[0].correct_answer);
+    // console.log(response.results[0].incorrect_answers);
+    // This almost pulls it but I can't figure out how to drill into what should be data.responseJSON.results...
+    // Maybe because the multpiple child thing
+    // need to filter the getdata to only get the values from the Json object that I specify 
+    // Need to use .append instead .text to fix the &quot; issue
+
+    // $.get("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple")
+    //     .then(function(response) {
+    //         console.log(response)
+
+    //     }
+
+    // let data = getdata();
+    // let test = JSON.stringify(data)
+
+    // };
 
 
-    // Need a function for showEverythingQuestion Section
+    // Need a function for showEverythingQuestion Section //
     // 1st the showEverything function will pull the first question (or random question that hasn't been selected yet if I can figure that out) from the API to populate the question display field. (This is a big challenge)
     // $.ajax({
     //     url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
@@ -44,57 +100,7 @@ $(function () {
     // if the user clicks another button then change the focus to that new btn they selected. 
 
 
-    // ORIGINAL //
-    // $.ajax({
-    //     url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
-    //     dataType: 'json',
-    //     method: "GET"
-    // }).then(function (response) {
-    //     // let questionOptions = {
-    //     //     "question 1": "(response.results[`0`].question",
-    //     //     "correct answer": "",
-    //     return (response.results);
-    // });
 
-    // WORKING CODE at the console level //
-    function getdata() {
-        var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // response.results.forEach(elem => {
-            //     let responseArray = $(); ... 
-
-            // })
-
-        });
-        // console.log(response.results[0].question);
-    };
-    getdata();
-    // Not working at the global level
-    console.log(response.results[0].question);
-    console.log(response.results[0].correct_answer);
-    console.log(response.results[0].incorrect_answers);
-    // This almost pulls it but I can't figure out how to drill into what should be data.responseJSON.results...
-    // Maybe because the multpiple child thing
-    // need to filter the getdata to only get the values from the Json object that I specify 
-    // Need to use .append instead .text to fix the &quot; issue
-
-    // $.get("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple")
-    //     .then(function(response) {
-    //         console.log(response)
-
-    //     }
-
-
-
-
-    // let data = getdata();
-    // let test = JSON.stringify(data)
-
-    // };
 
 
 

7edd7072fd280776d39cc915728474c1b32488be
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-28 14:48:04 -0400

    StartGame wrking = hides,shows,runsAJAX. Now push!

--- assets/javascript/app copy.js
+++ assets/javascript/app copy.js
@@ -2,11 +2,128 @@
 $(function () {
 
 
-    // Need a function for start btn click event that will call a few other functions into action.
-    // 1st this should hide the start btn
-    // 2nd then call another function named showEverythingQuestion Section.
-    // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
+    // NEW CODE BELOW THIS LINE ======================================================================
+    let currentQuestionIndex = 0;
 
+    const displayQuestions = function (questions) {
+        // by using the (questions) data we passed as an argument when the function is called, we can access the data inside the function
+        // renamed this input data from 'response.results' to question to avoid variable shadowing.  
+        console.log(questions[currentQuestionsIndex]);
+    };
+
+    const startGame = function () {
+        $.get('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple', function (respone) {
+            displayQuestions(respone.results)
+        });
+    };
+
+    // Make some lets here //
+    let questions = 0;
+    let answers = [];
+    let triviaData = [];
+
+    // From Elias //
+    // let currentAnswerSet = triviaData[currentQuestion].incorrect_answers;
+    // currentAnswerSet.push(triviaData[currentQuestion].correct_answer);
+
+
+    //Timer code from Emily //
+    let seconds = 30;
+
+    // function startCountDown() {
+
+    //     seconds--;
+
+    //     $($timer).text(`Time Left: ${seconds}`);
+
+    //     if (seconds === 0) {
+    //         clearTimeout(timer);
+    //         return false;
+    //     }
+    //     else {
+    //         setTimeout(countdown, 1000)
+    //     }
+    // }
+
+
+
+    // Get question into h3 class="questionField"// 
+    let $questionField = $(".questionField");
+    // Get question from index 0 of response.results
+
+    // AJAX request //
+
+    // WORKING CODE at the console level //
+    function getData() {
+        var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            // triviaData = response;
+            console.log(response.results);
+            console.log(response.results[0].question);
+            $(".questionField").html(response.results[0].question);
+            $(`.btn1`).html(response.results[`0`].incorrect_answers[0]);
+            $(`.btn2`).html(response.results[`0`].incorrect_answers[1]);
+            $(`.btn3`).html(response.results[`0`].incorrect_answers[2]);
+            $(`.btn4`).html(response.results[`0`].correct_answer);
+
+            // response.results.questions.forEach(elem => { })////////
+
+            // make your functions require arguments and when you call them in your ajax response, pass the data inside
+            // MY EXAMPLE: function addNumbers(a, b) { return a + b; ; } ..... x = addNumbers(1, 2);
+            // so it looks like we get an object with a property of 'results' that contains an array of 10 objects
+            // so i would make a fuction for a request for 10 questions for every new game. 
+            // then I would push all 10 'results' into a global variable
+
+            // Need a function for start btn click event that will call a few other functions into action.
+            // 1st this should hide the start btn // This is working now //
+            $(document).on('click', '#startButton', function startGame() {
+                $(this).hide();
+                // 2nd then call another function named showEverythingQuestion Section.
+                // Now add code to show the question section here.....
+                // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
+                // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
+                // startCountDown();
+                // $("#countDown").append((seconds));
+                // $(".submitAnswerButton");
+            });
+
+        });
+
+    };
+    getData();
+
+
+
+
+    // Need a function for showEverythingQuestion Section //
+    // 1st the showEverything function will pull the first question (or random question that hasn't been selected yet if I can figure that out) from the API to populate the question display field. (This is a big challenge)
+    // 2nd it pull from API to populate the option btns.(This is a big challenge)
+    // Performing GET requests to the API and logging the responses to the console
+
+    // NEED TO Push all incorrect and correct answers to an OPTIONS array. or current answer set. 
+
+    // NEED TO randomly assign btns1-4 from opptions array. (how do I do this? with a shuffle array function)
+    // click handler function to stop timer and check if the active radio button when submitAnswer is clicked is the (response.results[`1`].correct_answer);
+    // if yes, increase correct answer score, say congrats, and move to next questions 
+    // if wrong t hen increase incorrect answer score, say the correct answer was actually, and move to next question.
+    // let questions = ;
+    // [(response.results[`1`].correct_answer) + (response.results[`1`].incorrect_answers[0, 1, 2])]
+
+    // 3rd the showEverything function will also show the submitAnswer btn (maybe fade that in after 3 seconds or a nice animate effect)
+    // });
+    // Need a function that handles click events on the option BTN section
+    // this will activate only one btn at a time, likely though Bootstrap radio button stuff. (only one button active at a time is important)
+    // if the user clicks another button then change the focus to that new btn they selected. 
+
+
+
+
+
+    // OLD CODE ATTEMPTS BELOW THIS LINE ========================================================================
     // Need a function for showEverythingQuestion Section
     // 1st the showEverything function will pull the first question (or random question that hasn't been selected yet if I can figure that out) from the API to populate the question display field. (This is a big challenge)
     $.ajax({
@@ -18,13 +135,13 @@ $(function () {
         console.log(response.results[`1`].correct_answer);
         console.log(response.results[`1`].incorrect_answers[0, 1, 2]);
         console.log(response.results[`1`].question);
-        $(`.questionField`).text(response.results[`1`].question);
+        $(`.questionField`).html(response.results[`1`].question);
         // 2nd it pull from API to populate the option btns.(This is a big challenge)
         // Performing GET requests to the API and logging the responses to the console
-        $(`.btn1`).text(response.results[`1`].incorrect_answers[0]);
-        $(`.btn2`).text(response.results[`1`].incorrect_answers[1]);
-        $(`.btn3`).text(response.results[`1`].incorrect_answers[2]);
-        $(`.btn4`).text(response.results[`1`].correct_answer);
+        $(`.btn1`).html(response.results[`1`].incorrect_answers[0]);
+        $(`.btn2`).html(response.results[`1`].incorrect_answers[1]);
+        $(`.btn3`).html(response.results[`1`].incorrect_answers[2]);
+        $(`.btn4`).html(response.results[`1`].correct_answer);
         // Need to fix the regex pieces
         // replace(response.results[`1`].incorrect_answers[0, 1, 2]) = "";
 
@@ -42,36 +159,8 @@ $(function () {
         // this will activate only one btn at a time, likely though Bootstrap radio button stuff. (only one button active at a time is important)
         // if the user clicks another button then change the focus to that new btn they selected. 
 
-        // 10:38 trying to create an array from the API Response
-        $.ajax({
-            url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
-            dataType: 'json',
-            method: "GET"
-        }).then(function (response) {
-            // let questionOptions = {
-            //     "question 1": "(response.results[`0`].question",
-            //     "correct answer": "",
-            console.log(response.results);
-        });
-        // options.push(`(response.results[`1`].incorrect_answers[0]), (response.results[`1`].incorrect_answers[1]), (response.results[`1`].incorrect_answers[2]), (response.results[`1`].correct_answer);`);
-
-        // console.log(questionOptions);
-
-        // console.log(options);
-        // console.log(response.results[`1`].correct_answer);
-        // console.log(response.results[`1`].incorrect_answers[0, 1, 2]);
-        // console.log(response.results[`1`].question);
-        // $(`.questionField`).text
         // 2nd it pull from API to populate the option btns.(This is a big challenge)
-        // Performing GET requests to the API and logging the responses to the console
-        // $(`.btn1`).text(response.results[`1`].incorrect_answers[0]);
-        // $(`.btn2`).text(response.results[`1`].incorrect_answers[1]);
-        // $(`.btn3`).text(response.results[`1`].incorrect_answers[2]);
-        // $(`.btn4`).text(response.results[`1`].correct_answer);
-        // Need to fix the regex pieces
-        // replace(response.results[`1`].incorrect_answers[0, 1, 2]) = "";
 
-        // NEED TO Push all incorrect and correct answers to an OPTIONS array. 
         // NEED TO randomly assign btns1-4 from opptions array. (how do I do this? with a shuffle array function)
         // click handler function to stop timer and check if the active radio button when submitAnswer is clicked is the (response.results[`1`].correct_answer);
         // if yes, increase correct answer score, say congrats, and move to next questions 
@@ -82,30 +171,14 @@ $(function () {
         // 3rd the showEverything function will also show the submitAnswer btn (maybe fade that in after 3 seconds or a nice animate effect)
 
 
-
-
-        // WORKING ON THIS 10:09pm
         // Click Handler Function
-        // function handleClick() {
-        // something smart to handle clicks and make sure they select one answer from buttons 1-4 and then click submitAnswerButton
-        // $(`container.optionsBtns`).hasClass(`active`).on(`click`){
-        // if $(this) === (response.results[`1`].correct_answer) then log correct
-        // else $(this) != (response.results[`1`].correct_answer) then log incorrect
-        // }
-        // Just for noww...
-        // if ($(`.btn4`) === 
-        // }
+
         // If the player selects the correct answer, show a screen congratulating them for choosing the right option. 
         // After a few seconds, display the next question -- do this without user input.
-        // function answerChecker() {
-
-
+        // function answerChecker() {}
         // The scenario is similar for wrong answers and time - outs.
         // If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
         // If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer.Wait a few seconds, then show the next question.
-
-
-
 
 
         // Need a function that handles the submitAnswer btn click event (very complex, can this be done easier?)
@@ -123,18 +196,6 @@ $(function () {
         // 1st start the countDown Timer
         // 2nd run the showEverythingQuestion function to reuse that and to populate the question field and option buttons.
 
-
-
-        // result["0"];
-
-
-
-        // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
-        // $(div.countDown).setTimeout;
-        // $(div.submitAnswerButton);
-
-
-
         // On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
         // function finalScreen() {
         // count the number of correctGuesses, then append the html "correct guesses: correctGuesses".
@@ -145,34 +206,5 @@ $(function () {
 
 
     });
-
-    // let correctScore = correctGuess total;
-    // let incorrectScore = incorrectGuess total;
-
-    // the count down object
-    // let countDown = {
-    //     time: 0,
-    //     correctGuess: 0,
-    //     incorrectGuess: 0,
-    // }
-
-
-    // API for Medium level multipule choice video game questions src= https://opentdb.com/api_config.php
-    // https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple
-
-
-    // starting the game
-    // $("#startButton").on('click', function () {
-    // $("#startButton").slideToggle(300);
-    // countDown.setTimeout(() => {
-    // }, 30000);
-    // });
-    // startGame();
-    // use jQuery to manipulate the HTML, start the timer, produce a question, populate the buttons with answers.
-
-
-    // };
-
-    // };
 
 });
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,145 +1,102 @@
 // Short Document Ready
 $(function () {
-
     // Make some lets here //
+
     let questions = 0;
     let answers = [];
     let triviaData = [];
-    // From Elias //
-    // let currentAnswerSet = triviaData[currentQuestion].incorrect_answers;
-    // currentAnswerSet.push(triviaData[currentQuestion].correct_answer);
+    let questionSection = document.getElementById("questionSection");
 
-    // let thirtySeconds = setTimeout(1000 * 30);
-    //Timer code
-    let seconds = 30;
-
-    function startCountDown() {
-
-        seconds--;
-
-        $($timer).text(`Time Left: ${seconds}`);
-
-        if (seconds === 0) {
-            clearTimeout(timer);
-            return false;
-        }
-        else {
-            setTimeout(countdown, 1000)
-        }
-    }
-
-    // Need a function for start btn click event that will call a few other functions into action.
-    // 1st this should hide the start btn // This is working now //
+    // This click event runs the start game function. It hides the startButton on the HTML and then runs an AJAX Request
     $(document).on('click', '#startButton', function startGame() {
         $(this).hide();
-        // 2nd then call another function named showEverythingQuestion Section.
-        // Now add code to show the question section here.....
-        // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
-        // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
-        startCountDown();
-        $("#countDown").append((seconds));
-        $(".submitAnswerButton");
-    });
+        questionSection.classList.add("show-me");
+        {
+            var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (startGame) {
+                console.log(startGame.results);
+                createCategory(response); //create category from the response
 
-
-    // Get question into h3 class="questionField"// 
-    let $questionField = $(".questionField");
-    // Get question from index 0 of response.results
-
-    // AJAX request //
-
-    // WORKING CODE at the console level //
-    function getData() {
-        var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            // triviaData = response;
-            console.log(response.results);
-            console.log(response.results[0].question);
-            $(".questionField").html(response.results[0].question);
-            $(`.btn1`).html(response.results[`0`].incorrect_answers[0]);
-            $(`.btn2`).html(response.results[`0`].incorrect_answers[1]);
-            $(`.btn3`).html(response.results[`0`].incorrect_answers[2]);
-            $(`.btn4`).html(response.results[`0`].correct_answer);
-
-            // response.results.questions.forEach(elem => {
-
-            // })
+                questionArray.push(response.results); //Add the data to a question array
+            });
+            // Woking up to here, hides the start button, shows the questionSection, runs the ajax, console.log the ajax response.
+            // Not woking yet
+            let questions = function (questions) {
+                console.log(questions);
+            }
 
         });
 
-    };
-    getData();
-
-    // Maybe because the multpiple child thing
-    // need to filter the getdata to only get the values from the Json object that I specify 
-
-
-    // ORIGINAL //
-    // $.ajax({
-    //     url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
-    //     dataType: 'json',
-    //     method: "GET"
-    // }).then(function (response) {
-    //     // let questionOptions = {
-    //     //     "question 1": "(response.results[`0`].question",
-    //     //     "correct answer": "",
-    //     return (response.results);
-    // });
-
-
-    // };
-
-
-    // Need a function for showEverythingQuestion Section //
-    // 1st the showEverything function will pull the first question (or random question that hasn't been selected yet if I can figure that out) from the API to populate the question display field. (This is a big challenge)
-    // $.ajax({
-    //     url: `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`,
-    //     method: "GET"
-    // }).then(function (response) {
-    // console.log(response);
-    // console.log(response.results[`1`]);
-    // console.log(response.results[`1`].correct_answer);
-    // console.log(response.results[`1`].incorrect_answers[0, 1, 2]);
-    // console.log(response.results[`1`].question);
-    // $(`.questionField`).text(response.results[`1`].question);
-    // 2nd it pull from API to populate the option btns.(This is a big challenge)
-    // Performing GET requests to the API and logging the responses to the console
-    // $(`.btn1`).text(response.results[`1`].incorrect_answers[0]);
-    // $(`.btn2`).text(response.results[`1`].incorrect_answers[1]);
-    // $(`.btn3`).text(response.results[`1`].incorrect_answers[2]);
-    // $(`.btn4`).text(response.results[`1`].correct_answer);
-    // Need to fix the regex pieces
-    // replace(response.results[`1`].incorrect_answers[0, 1, 2]) = "";
-
-    // NEED TO Push all incorrect and correct answers to an OPTIONS array. or current answer set. 
-
-    // NEED TO randomly assign btns1-4 from opptions array. (how do I do this? with a shuffle array function)
-    // click handler function to stop timer and check if the active radio button when submitAnswer is clicked is the (response.results[`1`].correct_answer);
-    // if yes, increase correct answer score, say congrats, and move to next questions 
-    // if wrong t hen increase incorrect answer score, say the correct answer was actually, and move to next question.
-    // let questions = ;
-    // [(response.results[`1`].correct_answer) + (response.results[`1`].incorrect_answers[0, 1, 2])]
-
-    // 3rd the showEverything function will also show the submitAnswer btn (maybe fade that in after 3 seconds or a nice animate effect)
-    // });
-    // Need a function that handles click events on the option BTN section
-    // this will activate only one btn at a time, likely though Bootstrap radio button stuff. (only one button active at a time is important)
-    // if the user clicks another button then change the focus to that new btn they selected. 
+};
+        // startGame();
 
 
 
+        // From Bobby ======Not working, throwing a few errors about response, currentQuestionIndex, and potentially some jquery cdn issues===========
+        // const displayQuestions = function (questions) {
+        //     // by using the (questions) data we passed as an argument when the function is called, we can access the data inside the function
+        //     // renamed this input data from 'response.results' to question to avoid variable shadowing.  
+        //     console.log(questions[currentQuestionsIndex]);
+        // };
+
+        // const startGame = function () {
+        //     $.get('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple', function (response) {
+        //         displayQuestions(response.results)
+
+        //     });
+        //     console.log(response.results);
+        // };
+        // startGame();
+        // From Bobby =======================================
+
+        // /////////////////////////////////////////////////////////////////////////
+        // Older code, gets the console log to work ============= 
+        // Needs to be fixed with the input from Bobby(see slack), Elias(see slack and app copy.js file), and Emily(see slack)
+
+        // WORKING CODE at the console level //
+        // function getData() {
+        //     var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
+        //     $.ajax({
+        //         url: queryURL,
+        //         method: "GET"
+        //     }).then(function (response) {
+
+        //         // triviaData = response;
+        //         console.log(response.results);
 
 
+        //         // response.results.questions.forEach(elem => { })////////
 
+        //         // make your functions require arguments and when you call them in your ajax response, pass the data inside
+        //         // MY EXAMPLE: function addNumbers(a, b) { return a + b; ; } ..... x = addNumbers(1, 2);
+        //         // so it looks like we get an object with a property of 'results' that contains an array of 10 objects
+        //         // so i would make a fuction for a request for 10 questions for every new game. 
+        //         // then I would push all 10 'results' into a global variable
 
+        //         // Need a function for start btn click event that will call a few other functions into action.
+        //         // 1st this should hide the start btn // This is working now //
+        //         $(document).on('click', '#startButton', function startGame() {
+        //             $(this).hide();
+        //             // 2nd then call another function named showEverythingQuestion Section.
+        //             // Now add code to show the question section here.....
+        //             // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
+        //             // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
+        //             // startCountDown();
+        //             // $("#countDown").append((seconds));
+        //             // $(".submitAnswerButton");
+        //         });
+
+        //     });
+
+        // };
+        // getData();
 
 
 
 
+    });
 });
-
 

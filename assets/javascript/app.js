de17bcf50a8529976f4d0fb2c91558c2ca01d7c9
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-28 17:56:53 -0400

    Commit before Tutor Session

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,12 +5,28 @@ $(function () {
     let questions = 0;
     let answers = [];
     let triviaData = [];
-    let questionSection = document.getElementById("questionSection");
 
+    //Timer code from Emily //
+    let seconds = 30;
+    let $timeRemaining = document.getElementById("timeRemaining");
+    function startCountDown() {
+        seconds--;
+        $($timeRemaining).text(`Time Left: ${seconds}`);
+        if (seconds === 0) {
+            clearTimeout(timeRemaining);
+            return false;
+        }
+        else {
+            setTimeout(timeRemaining, 1000)
+        }
+    }
+    let ajaxArray = [];
+    let questionSection = document.getElementById("questionSection");
     // This click event runs the start game function. It hides the startButton on the HTML and then runs an AJAX Request
     $(document).on('click', '#startButton', function startGame() {
         $(this).hide();
         questionSection.classList.add("show-me");
+        startCountDown();
         {
             var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
             $.ajax({
@@ -18,24 +34,27 @@ $(function () {
                 method: "GET"
             }).then(function (startGame) {
                 console.log(startGame.results);
-                createCategory(response); //create category from the response
+                ajaxArray.push(startGame.results);
+                console.log(ajaxArray);
+                // Woking up to here, hides the start button, shows the questionSection, runs the ajax, console.log the ajax response.
+                let questionObj = $(ajaxArray); // transorming the array into an jQuery object
+                console.log(questionObj);
+                let qDcat = questionObj[0]; // from Yunus accessing the array when i created the array from the response.results created a key:0 + then the array
+                console.log(qDcat);
+                let question1 = qDcat[0].question; // creating a variable for question 1
+                console.log(question1);
 
-                questionArray.push(response.results); //Add the data to a question array
+
             });
-            // Woking up to here, hides the start button, shows the questionSection, runs the ajax, console.log the ajax response.
-            // Not woking yet
-            let questions = function (questions) {
-                console.log(questions);
-            }
-
-        });
-
-};
-        // startGame();
+        };
+    });
 
 
+});
 
-        // From Bobby ======Not working, throwing a few errors about response, currentQuestionIndex, and potentially some jquery cdn issues===========
+
+        // From Bobby ====================
+        // Not working, throwing a few errors about response, currentQuestionIndex, and potentially some jquery cdn issues===========
         // const displayQuestions = function (questions) {
         //     // by using the (questions) data we passed as an argument when the function is called, we can access the data inside the function
         //     // renamed this input data from 'response.results' to question to avoid variable shadowing.  
@@ -56,47 +75,15 @@ $(function () {
         // Older code, gets the console log to work ============= 
         // Needs to be fixed with the input from Bobby(see slack), Elias(see slack and app copy.js file), and Emily(see slack)
 
-        // WORKING CODE at the console level //
-        // function getData() {
-        //     var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
-        //     $.ajax({
-        //         url: queryURL,
-        //         method: "GET"
-        //     }).then(function (response) {
+      // response.results.questions.forEach(elem => { })////////
 
-        //         // triviaData = response;
-        //         console.log(response.results);
-
-
-        //         // response.results.questions.forEach(elem => { })////////
-
-        //         // make your functions require arguments and when you call them in your ajax response, pass the data inside
-        //         // MY EXAMPLE: function addNumbers(a, b) { return a + b; ; } ..... x = addNumbers(1, 2);
-        //         // so it looks like we get an object with a property of 'results' that contains an array of 10 objects
-        //         // so i would make a fuction for a request for 10 questions for every new game. 
-        //         // then I would push all 10 'results' into a global variable
-
-        //         // Need a function for start btn click event that will call a few other functions into action.
-        //         // 1st this should hide the start btn // This is working now //
-        //         $(document).on('click', '#startButton', function startGame() {
-        //             $(this).hide();
-        //             // 2nd then call another function named showEverythingQuestion Section.
-        //             // Now add code to show the question section here.....
-        //             // 3rd it will also call the startCountDown function which will use a setTimer thing for 30 seconds.
-        //             // question timmer, show only one question durring this timeout or until the user clicks a submitAnswerButton
-        //             // startCountDown();
-        //             // $("#countDown").append((seconds));
-        //             // $(".submitAnswerButton");
-        //         });
-
-        //     });
-
-        // };
-        // getData();
+                // make your functions require arguments and when you call them in your ajax response, pass the data inside
+                // MY EXAMPLE: function addNumbers(a, b) { return a + b; ; } ..... x = addNumbers(1, 2);
+                // so it looks like we get an object with a property of 'results' that contains an array of 10 objects
+                // so i would make a fuction for a request for 10 questions for every new game. 
+                // then I would push all 10 'results' into a global variable
 
 
 
 
-    });
-});
 

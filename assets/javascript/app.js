5165181e4fbf5b608f5eeeb33568d1f38d625378
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-29 14:28:18 -0400

    timer not working, moving on to Q&A section functs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,31 +2,77 @@
 $(function () {
     // Make some lets here //
 
-    let questions = 0;
-    let answers = [];
-    let triviaData = [];
+    // let questions = 0;
+    // let answers = [];
+    // let triviaData = [];
 
-    //Timer code from Emily //
-    let seconds = 30;
-    let $timeRemaining = document.getElementById("timeRemaining");
-    function startCountDown() {
+
+
+    // Time related functions. NEEDS HELP ============================
+    // Issues:
+    // goes negative instead of stopping the clock.
+    // needs a function for click handling to check the user's answer.
+    // needs to have functionality to move to the next question and display properly.
+    let seconds = 15;
+    let intervalId;
+    //  When the submit answer button gets clicked, run the stop function.
+    $("#submitAnswerBtn").on("click", stop);
+
+    function timeUp() {
+        clearTimeout(timeRemaining);
+        alert("Time Up!");
+        stop();
+        clearInterval(intervalId);
+        startCountDown();
+        // Move to next question....
+    };
+    function stop() {
+        clearTimeout(timeRemaining);
+        clearInterval(intervalId);
+        alert("Time stopped"); // doesnt actually stop...
+        console.log(seconds);
+
+        startCountDown();
+    };
+    function clearClock() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        clearClock();
         seconds--;
-        $($timeRemaining).text(`Time Left: ${seconds}`);
+        $($timeRemaining).html("<h5>" + "Time Left: " + seconds + "</h5>");
+        //  Once seconds hits zero...
         if (seconds === 0) {
-            clearTimeout(timeRemaining);
+            //  ...run the timeUp function.
+            timeUp();
+
             return false;
         }
         else {
-            setTimeout(timeRemaining, 1000)
-        }
-    }
+            setTimeout(timeRemaining, 15000)
+        };
+    };
+
+
+    let $timeRemaining = document.getElementById("timeRemaining");
+    function startCountDown() {
+        decrement();
+
+
+    };
+
+    // ===================================
+
+    // Main startGame function
     let ajaxArray = [];
     let questionSection = document.getElementById("questionSection");
     // This click event runs the start game function. It hides the startButton on the HTML and then runs an AJAX Request
     $(document).on('click', '#startButton', function startGame() {
-        $(this).hide();
-        questionSection.classList.add("show-me");
-        startCountDown();
+        $(this).hide(); // hide the start button
+        questionSection.classList.add("show-me"); // show the questionSection
+        startCountDown(); // calls the startCountDown Function to begin the timer
         {
             var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
             $.ajax({
@@ -35,55 +81,24 @@ $(function () {
             }).then(function (startGame) {
                 console.log(startGame.results);
                 ajaxArray.push(startGame.results);
-                console.log(ajaxArray);
                 // Woking up to here, hides the start button, shows the questionSection, runs the ajax, console.log the ajax response.
                 let questionObj = $(ajaxArray); // transorming the array into an jQuery object
                 console.log(questionObj);
-                let qDcat = questionObj[0]; // from Yunus accessing the array when i created the array from the response.results created a key:0 + then the array
-                console.log(qDcat);
-                let question1 = qDcat[0].question; // creating a variable for question 1
+                let questionCat = questionObj[0]; // Where this gets confusing again... from Yunus accessing the array when i created the array from the response.results created a key:0 + then the array
+                console.log(questionCat);
+                let question1 = questionCat[0].question; // creating a variable for question 1
                 console.log(question1);
+                // NEXT UP - figure out populating the answer buttons and refer to pseudo code
+                let correctAnswer1 = questionCat[0].correct_answer;
+                console.log(correctAnswer1);
+                let incorrectAnswers1 = questionCat[0].incorrect_answers;
+                console.log(incorrectAnswers1);
 
 
             });
         };
     });
-
+    console.log(ajaxArray);
 
 });
-
-
-        // From Bobby ====================
-        // Not working, throwing a few errors about response, currentQuestionIndex, and potentially some jquery cdn issues===========
-        // const displayQuestions = function (questions) {
-        //     // by using the (questions) data we passed as an argument when the function is called, we can access the data inside the function
-        //     // renamed this input data from 'response.results' to question to avoid variable shadowing.  
-        //     console.log(questions[currentQuestionsIndex]);
-        // };
-
-        // const startGame = function () {
-        //     $.get('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple', function (response) {
-        //         displayQuestions(response.results)
-
-        //     });
-        //     console.log(response.results);
-        // };
-        // startGame();
-        // From Bobby =======================================
-
-        // /////////////////////////////////////////////////////////////////////////
-        // Older code, gets the console log to work ============= 
-        // Needs to be fixed with the input from Bobby(see slack), Elias(see slack and app copy.js file), and Emily(see slack)
-
-      // response.results.questions.forEach(elem => { })////////
-
-                // make your functions require arguments and when you call them in your ajax response, pass the data inside
-                // MY EXAMPLE: function addNumbers(a, b) { return a + b; ; } ..... x = addNumbers(1, 2);
-                // so it looks like we get an object with a property of 'results' that contains an array of 10 objects
-                // so i would make a fuction for a request for 10 questions for every new game. 
-                // then I would push all 10 'results' into a global variable
-
-
-
-
 

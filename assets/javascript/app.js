047b08dd6227e5b093e070c98f084af4eb885546
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-29 23:08:40 -0400

    Commit to Submit. STILL A WORK IN PROGRESS!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,32 +18,41 @@ $(function () {
     // needs to have functionality to move to the next question and display properly.
     let seconds = 15;
     let intervalId;
+    let $timeRemaining = document.getElementById("timeRemaining");
+
+    function startCountDown() {
+        decrement();
+
+    };
     //  When the submit answer button gets clicked, run the stop function.
-    $("#submitAnswerBtn").on("click", stop);
+    $("#submitAnswerBtn").on("click", clearClock);
 
-    function timeUp() {
-        // clearTimeout(timeRemaining);
-        alert("Time Up!");
-        stop();
-        clearInterval(intervalId);
-        startCountDown();
-        // Move to next question....
-    };
-    function stop() {
+    function timeUp() {  // throws errors but does stop at 0....
         clearTimeout(timeRemaining);
+        console.log("Time Up!");
+        clearClock();
         clearInterval(intervalId);
-        alert("Time stopped"); // doesnt actually stop...
-        console.log(seconds);
 
-        startCountDown();
+        // then 
+        // Move to next question....
+        // startCountDown();
     };
+
+    // function stop() { // doesnt actually stop...
+    //     clearTimeout(timeRemaining);
+    //     clearInterval(intervalId);
+    //     console.log("Time stopped"); 
+    //     console.log(seconds);
+    //     startCountDown();
+    // };
+
     function clearClock() {
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
     }
 
     function decrement() {
-        clearClock();
+        // clearClock();
         seconds--;
         $($timeRemaining).html("<h5>" + "Time Left: " + seconds + "</h5>");
         //  Once seconds hits zero...
@@ -59,16 +68,65 @@ $(function () {
     };
 
 
-    let $timeRemaining = document.getElementById("timeRemaining");
-    function startCountDown() {
-        decrement();
+    // =============working on timer=======================
 
 
-    };
+    // let $timeRemaining = document.getElementById("timeRemaining");
+    // function startCountDown() {
+    //     decrement();
 
-    // ===================================
 
-    // Main startGame function
+    // function stop() {
+    //     clearTimeout(seconds);
+    //     console.log("Time stopped"); // doesnt actually stop...
+    //     console.log(seconds);
+    //     // Move to next question.....
+
+    //     startCountDown();
+    // };
+    // function clearClock() {
+    //     clearInterval(intervalId);
+    //     intervalId = setInterval(decrement, 1000);
+    // }
+
+    // function timeUp() {
+    //     clearClock();
+    //     $timeRemaining.hide();
+    // }
+
+    // function decrement() {
+    //     clearClock();
+    //     seconds--;
+    //     $($timeRemaining).html("<h5>" + "Time Left: " + seconds + "</h5>");
+    //     //  Once seconds hits zero...
+    //     if (seconds === 0) {
+    //         //  ...run the timeUp function.
+    //         return false;
+    //         timeUp();
+    //     }
+    //     else {
+    //         setTimeout(timeRemaining, 15000)
+    //     };
+    // };
+
+    // };
+
+    // =================================================================
+
+
+
+    // Main startGame functions
+
+    // function correctGuess () {
+
+    // }
+
+    // function incorrectGuess () {
+
+    // }
+
+
+
     let gameArray = [];
     let questionSection = document.getElementById("questionSection");
     // This click event runs the start game function. It hides the startButton on the HTML and then runs an AJAX Request
@@ -101,13 +159,14 @@ $(function () {
                 // need to shuffle the answers array before hitting html
                 // need to reset values of the answer btns before moving to next question
 
-                //     function shuffle(array) {
+                //     function shuffle(array) // might not be the best way to do this. I think 
+                // {
                 //     for (let i = array.length - 1; i > 0; i--) {
                 //         let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
                 //         [array[i], array[j]] = [array[j], array[i]]; // swap elements
                 //     }
                 // } 
-                // Pseudo code ===================================================================
+
                 // need a click event to check their submitted answer against the correctanswer data
                 // Function clickHandler for answer btns () (Not started)
                 //                 Reset timer and wait 7 seconds before starting to count down again.
@@ -137,7 +196,6 @@ $(function () {
 
     });
 
-    console.log(gameArray);
 
 });
 

8a26b5baaaf776bcdb16b8e71eb086c26a664f6b
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-30 20:26:24 -0400

    Organized code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,31 @@
 // Short Document Ready
 $(function () {
-    // Make some lets here //
-
-    let questions = 0;
-    let userAnswer;
-    let triviaData = [];
 
 
-
-    // Time related functions. 
-    // Issues:
-    // goes negative instead of stopping the clock.
-    // needs a function for click handling to check the user's answer.
-    // needs to have functionality to move to the next question and display properly.
-    // =============working on timer=======================
     let $timeRemaining = $("#timeRemaining");
-    let $submitAnswer = $("#submitAnswerBtn");
     let $startButton = $("#startButton");
+    let score = 0;
     let seconds = 15;
+    let randomQuestion;
+    let thisQuestion = 0;
+    let answers = [];
+    let correctAnswer;
+    let gameArray = [];
+    let $questionSection = $("#questionSection");
+    let $finalScore = $("#finalScore");
 
     function countDown() {
         intervalId = setInterval(decrement, 1000);
-        console.log("start interval counter");
     }
 
     function decrement() {
         seconds--;
-        // console.log(seconds);
         if (seconds < 15) {
-            // console.log(`We made it to the timereaming dom if`);
             $timeRemaining.html("<h5>" + "Time Left: " + seconds + "</h5>");
         }
         if (seconds < 1) {
-            NewQuestion(gameArray);
+            newQuestion(gameArray);
         }
-        // $submitAnswer.on("click", stop); // clicks multiple times.
     }
 
     function stop(results) {
@@ -43,40 +34,31 @@ $(function () {
         $("#finalScore").text(`Your final score is ${score}/10`)
         if (results) {
             console.log("win");
-            $("#finalScore").addClass(".show-me");
+            $finalScore.addClass(".show-me");
         }
         else {
             console.log("lose");
-            questionSection.classList.add("hide-me")
+            $questionSection.addClass("hide-me")
         }
-
-        // display the correct answer
-        // wait 5 seconds but while it waits..
-        // run NewQuestion();
-        // hide the section Div;
-        // show "total: " + score + "/10"(out of 10)
-        // show startGamebtn
-
-
     }
-    let score = 0;
+
     function guessCheck(data) {
         console.log(data);
         console.log("this is guess check");
         if (correctAnswer === data) {
             score++;
-            NewQuestion(gameArray);
+            newQuestion(gameArray);
         }
+        // hide the section Div;
+        // display the correct answer inbetween questions
+        // wait 5 seconds but while it waits run NewQuestion
         else {
-            NewQuestion(gameArray);
+            newQuestion(gameArray);
 
         }
     }
 
-
-
-
-    function NewQuestion(game) {
+    function newQuestion(game) {
         if (gameArray.length > 0) {
             clearInterval(intervalId);
             seconds = 15;
@@ -105,56 +87,38 @@ $(function () {
         }
         else {
             stop(true);
-            questionSection.classList.remove("show-me");
-            finalScore.classList.remove("hide-me");
+            $questionSection.removeClass("show-me");
+            $finalScore.removeClass("hide-me");
+            $startButton.show();
         }
     }
 
-
     // Main startButton functions
-    let randomQuestion;
-    let thisQuestion = 0;
-    let answers = [];
-    let correctAnswer;
-    let gameArray = [];
-    let questionSection = document.getElementById("questionSection");
-    let finalScore = document.getElementById("finalScore");
+
     // This click event runs the start game function. It hides the startButton on the HTML and then runs an AJAX Request
     $startButton.on("click", function () {
-        $(this).hide(); // hide the start button
-        questionSection.classList.add("show-me"); // show the questionSection
-        countDown(); // calls the CountDown Function to begin the timer
-        // also need to navigate to index[0] and push out the data to the buttons
+        $(this).hide();
+        score = 0;
+        $finalScore.addClass("hide-me");
+        $questionSection.addClass("show-me");
+        countDown();
         {
             var queryURL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (renderGame) {
-                // console.log(renderGame.results);
                 gameArray = (renderGame.results);
-                // console.log(renderGame);
-                // console.log(gameArray[0].question)
-                NewQuestion(gameArray)
-
-
+                newQuestion(gameArray)
                 $(".btn").on('click', function () {
                     _this = $(this)
                     guessCheck(_this.attr("data-value"));
                     console.log(_this);
-
-                })
-
-
-                // Write a Readme(not started)
-
-
+                });
             });
         };
-
-
     });
 
-
+    // need to write a read me
 });
 
